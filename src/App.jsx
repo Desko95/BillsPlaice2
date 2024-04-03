1791b44fb4bfdf3b0ec3008e1eb6ee375e75b23a
Simone <simonedescode@gmail.com>
2024-04-03 16:11:00 +0100

    TEMPLATE FOR MULTIPAGE WEBSITES NAVBAR + FOOTER)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/header/Navbar";
+import Footer from "./components/footer/Footer";
 import Home from "./pages/1.home/Home";
 import Products from "./pages/2.products/Products";
 import Team from "./pages/3.team/Team";
@@ -13,14 +14,15 @@ function App() {
         <Navbar />
 
         <Routes>
-          
+
           <Route exact path="/" element={<Home/>} />
           <Route exact path="/products" element={<Products/>} />
           <Route exact path="/team" element={<Team/>} />
           <Route exact path="/contact" element={<Contact/>} />
 
-
         </Routes>
+
+        <Footer/>
 
       </Router>
     </div>
